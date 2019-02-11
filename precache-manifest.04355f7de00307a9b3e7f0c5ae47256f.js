307c92cc83ab8fd64b65dcca8b28bbb73158d73f
Shayne <akili.shayne@gmail.com>
2019-02-11 12:38:55 -0500

    Pages setup

---
+++ precache-manifest.04355f7de00307a9b3e7f0c5ae47256f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "c7ed199f8f8e895d307d",
+    "url": "/static/js/main.c7ed199f.chunk.js"
+  },
+  {
+    "revision": "1eff3bbdfa2e1a94bbd6",
+    "url": "/static/js/2.1eff3bbd.chunk.js"
+  },
+  {
+    "revision": "c7ed199f8f8e895d307d",
+    "url": "/static/css/main.e808fb44.chunk.css"
+  },
+  {
+    "revision": "de7100a0f198372e23893625deacbc95",
+    "url": "/index.html"
+  }
+];
